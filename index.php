48b548274f7bca79b6f38880acd0d7d011ef80d1
SalmaMei <140433654+SalmaMei@users.noreply.github.com>
2023-07-25 00:11:21 +0700

    SalmaMei_Toko Buku

--- index.php
+++ index.php
@@ -1,103 +1,161 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Pendataan Buku</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="style.css" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>TOKO BUKU</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/7.png" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
 </head>
-<body>
-<nav class="navbar navbar-expand-lg shadow navbar-white fixed-top" style="background-color: #bde6f1">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">B.U.K.U</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top">TOKO BUKU MEI-MEI</a>
+            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Data</a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a
+              >
             </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Tentang</a>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about"
+                >About</a
+              >
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> About </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Ulasan</a></li>
-                <li><a class="dropdown-item" href="#">dll</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                <li class="nav-item dropend">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black"> Cek ulang </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Penilaian</a></li>
-                    <li><a class="dropdown-item" href="#">dll</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-              </ul>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact"
+                >Contact</a
+              >
             </li>
           </ul>
         </div>
-        <button type="submit" class="btn btn-primary justify-content-end">Sign in</button>
       </div>
     </nav>
-    <!--tutup nav-->
-    <!--card-->
-    <div class="container">
-      <div class="row text-center mb-4">
-        <div class="col">
-          <h2>Ayo Baca Buku</h2>
+    <!-- Masthead-->
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+        <img
+          class="masthead-avatar mb-5"
+          src="assets/img/5.jpg"
+          alt="..."
+        />
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">TOKO BUKU MEI-MEI</h1>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+          <div class="divider-custom-line"></div>
+          <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+          <div class="divider-custom-line"></div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 mb-4">
-          <div class="card">
-            <img src="img/project/1.jpg" class="card-img-top" alt="1" />
-            <div class="card-body">
-              <p class="card-text">Buku-buku bahasa Jepang mencakup karya sastra dan lain lain</p>
-            </div>
-          </div>
+        
+    <!-- Portfolio Section-->
+    <section class="page-section portfolio" id="portfolio">
+      <div class="container">
+        <!-- Portfolio Section Heading-->
+        <h2
+          class="page-section-heading text-center text-uppercase text-secondary mb-0"
+        >
+          Portfolio
+        </h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+          <div class="divider-custom-line"></div>
+          <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+          <div class="divider-custom-line"></div>
         </div>
-        <div class="col-md-4 mb-4">
-          <div class="card">
-            <img src="img/project/2.jpg" class="card-img-top" alt="2" />
-            <div class="card-body">
-              <p class="card-text">Buku self development yang akan membantu anda mengembangkan diri anda</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-4">
-          <div class="card">
-            <img src="img/project/3.jpg" class="card-img-top" alt="3" />
-            <div class="card-body">
-              <p class="card-text">Buku keuangan yang akan membantu memanagement keuangan keseharian anda</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- tutup card-->
-    
-  <div class="row justify-content-center">
-    <div class="col-xl-12 col-lg-12 col-md-12">
-        <div class="card shadow-sm my-0">
-            <div class="card-body p-4">
-                <div class="row">
-                   <div class="col-lg-4">
-                    <?php include"form.php";?>
-                   </div>
-                   <div class="col-lg-8">
-                    <?php include"data.php";?>
-</div>
+        <!-- Portfolio Grid Items-->
+        <div class="row justify-content-center">
+          <!-- Portfolio Item 1-->
+          <div class="col-md-6 col-lg-4 mb-5">
+            <div
+              class="portfolio-item mx-auto"
+              data-bs-toggle="modal"
+              data-bs-target="#portfolioModal1"
+            >
+              <div
+                class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"
+              >
+                <div
+                  class="portfolio-item-caption-content text-center text-white"
+                >
+                  <i class="fas fa-plus fa-3x"></i>
                 </div>
+              </div>
+              <img class="img-fluid" src="assets/img/4.jpg" alt="Buku-Buku Bahasa Korea dan sastra lainnya" />
             </div>
-        </div>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+          </div>
+          <!-- Portfolio Item 2-->
+          <div class="col-md-6 col-lg-4 mb-5">
+            <div
+              class="portfolio-item mx-auto"
+              data-bs-toggle="modal"
+              data-bs-target="#portfolioModal2"
+            >
+              <div
+                class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"
+              >
+                <div
+                  class="portfolio-item-caption-content text-center text-white"
+                >
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="assets/img/2.jpg" alt="Buku-Buku Self" />
+            </div>
+          </div>
+          <div class="container">
+            <div class="row text-center mb-4">
+              <div class="col">
+                <h2>Ayo Baca Buku</h2>
+              </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-xl-12 col-lg-12 col-md-12">
+                    <div class="card shadow-sm my-0">
+                        <div class="card-body p-4">
+                            <div class="row">
+                               <div class="col-lg-4">
+                                <?php include"form.php";?>
+                               </div>
+                               <div class="col-lg-8">
+                                <?php include"data.php";?>
+            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>      
+           
+       
+            
+
+                            <!-- Bootstrap core JS-->
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+                            <!-- Core theme JS-->
+                            <script src="js/scripts.js"></script>
+                            <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+                            <!-- * *                               SB Forms JS                               * *-->
+                            <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+                            <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+                            <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
+
 </html>
---
+++ perpustakaan.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2023 at 06:15 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `idBuku` int(255) NOT NULL,
+  `judul` text NOT NULL,
+  `pengarang` text NOT NULL,
+  `tahun_terbit` text NOT NULL,
+  `isbn` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`idBuku`, `judul`, `pengarang`, `tahun_terbit`, `isbn`) VALUES
+(1001, 'Kelinci', 'jaemin', '2005', 33);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`idBuku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
